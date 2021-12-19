16cea253517d4282b457a7045457d11f02a5932b
Cristiano <neivacristiano@yahoo.com.br>
2021-12-19 17:54:32 -0300

    feat: add task list

--- tarefas.js
+++ tarefas.js
@@ -4,9 +4,24 @@ class Tarefa {
         this.categoria = categoria
         this.realizada = realizada
     }
+
+    adicionaNaPagina() {
+        const taskList = document.querySelector('#lista-tarefas');
+        
+        const newTask = document.createElement('li');
+        newTask.classList.add('item-tarefa', `categoria-${this.categoria}`);
+
+        if (this.realizada) newTask.classList.add('marcado');
+
+        newTask.innerHTML = this.nome;
+       
+        taskList.appendChild(newTask);
+    }
 }
 
 const tarefas = [
     new Tarefa('Comprar leite', 'compras', false), 
     new Tarefa('Escutar chimbinha', 'lazer', true)
 ];
+
+tarefas.forEach(task => task.adicionaNaPagina())
