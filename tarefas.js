1db9ea2ae40707472cea127eeea58ae4fb8fb507
Cristiano <neivacristiano@yahoo.com.br>
2021-12-21 21:48:06 -0300

    feat: add conclusion

--- tarefas.js
+++ tarefas.js
@@ -1,64 +1,73 @@
 class Tarefa {
-    constructor(nome, categoria, realizada) {
-        this.nome = nome
-        this.categoria = categoria
-        this.realizada = realizada
-    }
+  constructor(nome, categoria, realizada) {
+    this.nome = nome;
+    this.categoria = categoria;
+    this.realizada = realizada;
+  }
 
-    adicionaNaPagina() {
-        const taskList = document.querySelector('#lista-tarefas');
-        
-        const newTask = document.createElement('li');
-        newTask.classList.add('item-tarefa', `categoria-${this.categoria}`);
+  adicionaNaPagina() {
+    const taskList = document.querySelector("#lista-tarefas");
 
-        if (this.realizada) newTask.classList.add('marcado');
+    const newTask = document.createElement("li");
+    newTask.classList.add("item-tarefa", `categoria-${this.categoria}`);
 
-        newTask.innerHTML = this.nome;
-       
-        taskList.appendChild(newTask);
-    }
+    if (this.realizada) newTask.classList.add("marcado");
+
+    newTask.innerHTML = this.nome;
+
+    novaTarefa.addEventListener("click", concluiTarefa);
+
+    taskList.appendChild(newTask);
+  }
 }
 
 const tarefas = [
-    new Tarefa('Comprar leite', 'compras', false), 
-    new Tarefa('Escutar chimbinha', 'lazer', true)
+  new Tarefa("Comprar leite", "compras", false),
+  new Tarefa("Escutar chimbinha", "lazer", true),
 ];
 
-tarefas.forEach(task => task.adicionaNaPagina())
+tarefas.forEach((task) => task.adicionaNaPagina());
 
 function incluiTarefa(event) {
-    const nome = document.querySelector('#nova-tarefa-nome');
+  const nome = document.querySelector("#nova-tarefa-nome");
 
-    if (!nome) return
-    
-    const categoria = document.querySelector('#nova-tarefa-categoria');
-    
-    const tarefa = new Tarefa(nome.value, categoria.value, false);
-    
-    tarefas.push(tarefa);
-    tarefa.adicionaNaPagina();
-    
-    nome.value = '';
-    categoria.value = 'lazer';
+  if (!nome) return;
+
+  const categoria = document.querySelector("#nova-tarefa-categoria");
+
+  const tarefa = new Tarefa(nome.value, categoria.value, false);
+
+  tarefas.push(tarefa);
+  tarefa.adicionaNaPagina();
+
+  nome.value = "";
+  categoria.value = "lazer";
 }
 
-document.querySelector('#incluir-nova-tarefa').addEventListener('click', incluiTarefa);
+document
+  .querySelector("#incluir-nova-tarefa")
+  .addEventListener("click", incluiTarefa);
 
-document.querySelector('#filtro-de-categoria').addEventListener('change', (event) => {
+document
+  .querySelector("#filtro-de-categoria")
+  .addEventListener("change", (event) => {
     const categoria = event.currentTarget.value;
-    const listaDeTarefas = document.querySelectorAll('#lista-tarefas li');
-    
+    const listaDeTarefas = document.querySelectorAll("#lista-tarefas li");
+
     if (categoria) {
-        for (const tarefa of listaDeTarefas) {
-            if (tarefa.classList.contains(`categoria-${categoria}`)) {
-                tarefa.classList.remove('retido-no-filtro');
-            }
-            else tarefa.classList.add('retido-no-filtro');
-        }
-        return
+      for (const tarefa of listaDeTarefas) {
+        if (tarefa.classList.contains(`categoria-${categoria}`)) {
+          tarefa.classList.remove("retido-no-filtro");
+        } else tarefa.classList.add("retido-no-filtro");
+      }
+      return;
     }
 
     for (const tarefa of listaDeTarefas) {
-        tarefa.classList.remove('retido-no-filtro');
+      tarefa.classList.remove("retido-no-filtro");
     }
-});
+  });
+
+function concluiTarefa(event) {
+  event.currentTarget.classList.toggle("marcado");
+}
